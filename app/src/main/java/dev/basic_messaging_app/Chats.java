f163652d4e399eaa8cbe438d6d5747ee1236efd6
rottiakash <43717372+rottiakash@users.noreply.github.com>
2018-10-24 13:03:49 +0530

    Added java intent for signin

--- app/src/main/java/dev/basic_messaging_app/Chats.java
+++ app/src/main/java/dev/basic_messaging_app/Chats.java
@@ -1,7 +1,11 @@
 package dev.basic_messaging_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Chats extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class Chats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
+        Intent signin=new Intent(this,Auth.class);
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivity(signin);
+        }
+        else
+        {
+            Toast.makeText(this,
+                    "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+            displayChatMessages();
+        }
+    }
+    private void displayChatMessages()
+    {
+
     }
 }
