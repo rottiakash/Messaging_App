034f7753dd7d0bf8e0afd7ab64d50cd85ed17176
rottiakash <43717372+rottiakash@users.noreply.github.com>
2018-10-26 21:37:53 +0530

    Post method added

--- app/src/main/java/dev/basic_messaging_app/Chats.java
+++ app/src/main/java/dev/basic_messaging_app/Chats.java
@@ -6,12 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Chats extends AppCompatActivity {
 
@@ -97,5 +100,19 @@ public class Chats extends AppCompatActivity {
                     });
         }
         return true;
+    }
+
+    public void OnclickButton(View view)
+    {
+        EditText input=(EditText) findViewById(R.id.editText);
+        FirebaseDatabase.getInstance()
+                .getReference()
+                .push()
+                .setValue(new ChatMessage(input.getText().toString(),
+                        FirebaseAuth.getInstance()
+                                .getCurrentUser()
+                                .getDisplayName())
+                );
+        input.setText("");
     }
 }
